754ed38f3638a102dd41a613764416ec0123ef7b
Anton Misskii <amisskii@redhat.com>
2025-11-19 10:29:02 +0100

    fix: podman dowload url
    Signed-off-by: Anton Misskii <amisskii@redhat.com>

--- plans/scripts/install-podman.sh
+++ plans/scripts/install-podman.sh
@@ -6,7 +6,6 @@ sudo dnf remove -y podman
 
 # Construct the download URL for the specific Podman version.
 COMPOSE_VERSION="fc$(echo "$COMPOSE" | cut -d'-' -f2)"
-CUSTOM_PODMAN_URL="https://kojipkgs.fedoraproject.org//packages/podman/${PODMAN_VERSION}/1.${COMPOSE_VERSION}/${ARCH}/podman-${PODMAN_VERSION}-1.${COMPOSE_VERSION}.${ARCH}.rpm"
 
 # Install Podman based on the requested version:
 #   - "nightly": latest nightly build from rhcontainerbot/podman-next COPR repository
@@ -23,6 +22,7 @@ else
     if [[ "$PODMAN_VERSION" == "latest" ]]; then
         PODMAN_VERSION="$(curl -s https://api.github.com/repos/containers/podman/releases | jq -r '.[] | select(.prerelease == false) | .tag_name' | head -n1 | sed 's/^v//')"
     fi
+    CUSTOM_PODMAN_URL="https://kojipkgs.fedoraproject.org//packages/podman/${PODMAN_VERSION}/1.${COMPOSE_VERSION}/${ARCH}/podman-${PODMAN_VERSION}-1.${COMPOSE_VERSION}.${ARCH}.rpm"
     curl -Lo podman.rpm "$CUSTOM_PODMAN_URL"
     sudo dnf install -y ./podman.rpm
     rm -f podman.rpm
